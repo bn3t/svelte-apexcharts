10a8595d3b8f88ad7d3c406a8d62df9cd38d49c8
Bernard Niset <bernard.niset@gmail.com>
2022-07-11 20:00:04 +0200

    chore: Deploy to github pages via actions

--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,16 @@
 import { expect, test } from '@playwright/test';
 
+const ROOT_URL = '/svelte-apexcharts/';
+
 test.describe('Chart Manipulation', () => {
   test('A chart is displayed', async ({ page }) => {
-    await page.goto('/');
+    await page.goto(ROOT_URL);
     const chart = page.locator('#apexchartschart1');
     await expect(chart).toBeVisible();
   });
 
   test('Chart can toggled visible', async ({ page }) => {
-    await page.goto('/');
+    await page.goto(ROOT_URL);
 
     await page.locator('button', { hasText: 'Toggle chart visibility' }).click();
 
