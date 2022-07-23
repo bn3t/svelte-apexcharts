92ef5b83ce015f57783d5220043dca946996c1f9
Bernard Niset <bernard.niset@gmail.com>
2022-07-23 12:03:12 +0200

    feat: improve demo app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -25,11 +25,15 @@
 <div class="root">
   <ApexChart {options} {series} {animate} />
 </div>
-<div><a href="{base}/complex" alt="Access a more complex example">A more complex example</a></div>
+<div>
+  <a href="{base}/complex" alt="Access a more complex example">
+    <button>Go to a more complex example</button>
+  </a>
+</div>
 
 <style>
   .root {
     width: 100%;
-    height: 800px;
+    height: 600px;
   }
 </style>
