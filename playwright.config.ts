be597c06b21cba51f9aaa78e4c43e90b142641b0
Bernard Niset <bernard.niset@gmail.com>
2022-08-17 11:02:18 +0200

    chore: Improve Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,21 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
+const localDev = !process.env.DEV;
+
+const baseURL = localDev ? 'http://127.0.0.1:4173/svelte-apexcharts/' : 'http://127.0.0.1:5173';
+
 const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173,
-    timeout: 20000
-  }
+  use: {
+    baseURL
+  },
+  webServer: localDev
+    ? {
+        command: (process.env.CI ? '' : 'npm run build && ') + 'npm run preview',
+        port: 4173,
+        timeout: process.env.CI || process.env.DEV ? undefined : 20000
+      }
+    : undefined,
+  reporter: process.env.CI ? 'html' : 'list'
 };
 
 export default config;
--- tests/complex.test.ts
+++ tests/complex.test.ts
@@ -1,15 +1,15 @@
 import { expect, test } from '@playwright/test';
 
-const ROOT_URL = '/svelte-apexcharts/complex';
+const ROOT_URL = './complex';
 
-test.describe('Chart Manipulation', () => {
+test.describe('Chart Manipulation - Complex', () => {
   test('A chart is displayed', async ({ page }) => {
     await page.goto(ROOT_URL);
     const chart = page.locator('#apexchartschart1');
     await expect(chart).toBeVisible();
   });
 
-  test('Chart can toggled visible', async ({ page }) => {
+  test('Chart can be toggled visible', async ({ page }) => {
     await page.goto(ROOT_URL);
 
     await page.locator('button', { hasText: 'Toggle chart visibility' }).click();
